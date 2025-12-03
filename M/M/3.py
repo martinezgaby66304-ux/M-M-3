1df1c2d2da95ad8e44a2abff6c62017d54945050
martinezgaby66304 <martinezgaby66304@gmail.com>
2025-12-02 19:26:44 -0600

    Primer formula: Calcula la Utilizacion

--- M/M/3.py
+++ M/M/3.py
@@ -3,9 +3,14 @@ class MM3:
         self.landa = landa
         self.miu = miu
         self.c = c
+    def calcular_utilizacion(self):
+        return self.landa / (self.c * self.miu)
 
-    landa_promedio_llegada = float(input("Ingrese la tasa de llegada (landa): "))
-    miu_promedio_servicio = float(input("Ingrese la tasa de servicio (miu): "))
-    c = int(input("Ingrese el número de servidores (c): ")) 
-    
-                                          
+
+landa_promedio_llegada = float(input("Ingrese la tasa de llegada (landa): "))
+miu_promedio_servicio = float(input("Ingrese la tasa de servicio (miu): "))
+c = int(input("Ingrese el número de servidores (c): ")) 
+
+modelo = MM3(landa_promedio_llegada,miu_promedio_servicio,c)  
+
+print("p(utilizacion):", modelo.calcular_utilizacion())
